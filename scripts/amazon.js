4badb3da0c47cfe876e9b5b92761497782c2235d
lxn <thereallxn1@gmail.com>
2025-01-08 20:36:21 +0300

    added cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -4,8 +4,7 @@ products.forEach((product) => {
   productsHTML += `
         <div class="product-container">
             <div class="product-image-container">
-                <img
-                class="product-image"
+                <img class="product-image"
                 src="${product.image}"
                 />
             </div>
@@ -30,29 +29,49 @@ products.forEach((product) => {
 
             <div class="product-quantity-container">
                 <select>
-                <option selected value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
+                    <option selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
                 </select>
             </div>
 
             <div class="product-spacer"></div>
 
             <div class="added-to-cart">
-                <img src="images/icons/checkmark.png" />
-                Added
+                <img src="images/icons/checkmark.png" />Added
             </div>
 
-            <button class="add-to-cart-button button-primary">Add to Cart</button>
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">Add to Cart</button>
         </div>
   `;
 });
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+    const productQuantity = 2;
+    let matchingItem;
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({ productId: productId, quantity: 1 });
+    }
+
+    console.log(cart);
+  });
+});
